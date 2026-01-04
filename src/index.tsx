776905089ca6fd2dd3ee26dbff2ddda165639750
Koalaa <ashleela0810@gmail.com>
2026-01-04 20:23:36 +0000

    Again it did it let see what happens

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Include if you have this file
 
 // Create root element
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
@@ -13,12 +14,13 @@ if (rootElement) {
     </React.StrictMode>
   );
 } else {
-  console.error("Root element not found!");
+  console.error('Root element not found!');
+  
   // Create root div if it doesn't exist
-  const newRoot = document.createElement("div");
-  newRoot.id = "root";
+  const newRoot = document.createElement('div');
+  newRoot.id = 'root';
   document.body.appendChild(newRoot);
-
+  
   const root = ReactDOM.createRoot(newRoot);
   root.render(
     <React.StrictMode>
